52aa5aa6562add064cd6e24f55b53d0b250e91eb
Shubham Thakur <shubham58188@gmail.com>
2024-03-29 22:30:00 +0530

    check api

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,9 +4,12 @@ import axios from "axios";
 
 const App = ()=>{
 
-    
-
+    const [isData,setIsData] = useState(false);
     const [city,setCity] = useState("");
+    const [location,setLocation] = useState("");
+    const [temp,setTemp] = useState("");
+    const [weatherDescription,setWeatherDescription] = useState("");
+    const [iconUrl,setIconUrl] = useState("");
     const apiKey = process.env.REACT_APP_API_KEY;
 
     const handleChange = (e)=>{
@@ -18,12 +21,18 @@ const App = ()=>{
         e.preventDefault();
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-        axios.get(url).
-        then((response)=>{
-
-            const weatherData = JSON.parse(response);
-            console.log(weatherData.main.temp);
-
+        axios.get(url)
+        .then((response)=>{
+            // console.log(response);
+            const weatherData = response.data;
+            console.log(weatherData);
+            setLocation(city);
+            setTemp(weatherData.main.temp);
+            setWeatherDescription(weatherData.weather[0].description);
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+            setIconUrl(iconUrl);
+            setIsData(true);
         })
 
     }
@@ -34,6 +43,14 @@ const App = ()=>{
                 <input type="text" value={city} onChange={handleChange} />
                 <input type="submit"/>
             </form>
+
+            {isData && <div>
+                <h1>The weather in {location} is {temp} degree celcius</h1>
+            </div>}
+            {/* <div>
+                <h1>{`The weather in ${city} is ${temp} degree celcius`}</h1>
+            </div> */}
+
         </div>
     )
 };
